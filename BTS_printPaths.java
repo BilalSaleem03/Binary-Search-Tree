b226580a5045b0eaef6ae011cdf1fff71256e7e1
BilalSaleem03 <155255932+BilalSaleem03@users.noreply.github.com>
2024-02-07 16:30:47 +0500

    Add files via upload

---
+++ BTS_printPaths.java
@@ -0,0 +1,179 @@
+import java.util.*;
+public class BTS_printPaths {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root , int val)
+    {
+        if(root == null)
+        {
+            root = new Node(val);
+            return root;
+        }
+        if(root.data > val)
+        {
+            root.left = insert(root.left , val);
+        }
+        else
+        {
+            root.right = insert(root.right , val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+    public static boolean search(Node root , int find)
+    {
+        
+        if(root == null)
+        {
+            return false;
+        }
+        if(root.data > find)
+        {
+            return search(root.left , find);
+        }
+        else if(root.data == find)
+        {
+            return true;
+        }
+        else
+        {
+            return search(root.right , find);
+        }
+        //return false;
+    }
+
+    //delete
+/* 
+    public static Node delete(Node root , int val)
+    {
+        if(root.data>val)
+        {
+            root.left = delete(root.left , val);
+        }
+        else if(root.data< val)
+        {
+            root.right = delete(root.right , val);
+        }
+        else //found
+        {
+            //case 1
+            if(root.left == null && root.right == null)
+            {
+                return null;
+            }
+            //case 2
+            if(root.left == null)
+            {
+                return root.right;
+            }
+            if(root.right == null)
+            {
+                return root.left;
+            }
+            //case 3
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right , IS.data);
+        }
+        return root;
+    }
+
+    public static Node inorderSuccessor(Node root)
+    {
+        while(root.left != null)
+        {
+            root = root.left;
+        }
+        return root;
+    }
+    
+    public static void printInRange(Node root , int x , int y)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        if(root.data <= y && root.data >= x)
+        {
+            printInRange(root.left , x , y);
+            System.out.print(root.data+" ");
+            printInRange(root.right , x , y);
+        }
+        else if(root.data >= x)
+        {
+            printInRange(root.left , x , y);
+        }
+        else
+        {
+            printInRange(root.right ,x ,y);
+        }
+    }
+    */
+
+    public static void print(ArrayList<Integer> path)
+    {
+        for(int i = 0 ; i<path.size(); i++)
+        {
+            System.out.print(path.get(i)+" ");
+        }
+        System.out.println();
+    }
+
+    public static void paths2Leaf(Node root , ArrayList<Integer> path)
+    {
+        if(root == null)
+        {
+            return;
+        }
+
+        path.add(root.data);
+
+        if(root.left == null && root.right== null)
+        {
+            print(path);
+        }
+        else
+        {
+            paths2Leaf(root.left , path);
+            paths2Leaf(root.right , path);
+        }
+        path.remove(path.size()-1);
+    }
+    public static void main(String [] args)
+    {
+        int values[] = {8,5,3,1,4,6,10,11,14};
+        Node root = null;
+        for(int i = 0 ; i<values.length;i++)
+        {
+            root = insert(root , values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        //System.out.println(search(root , 7));
+        //delete(root , 5);
+        //inorder(root);
+        //printInRange(root , 3,10);
+        paths2Leaf(root , new ArrayList<>());
+    }
+}
